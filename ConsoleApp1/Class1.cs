74dfdf8f868bc3e6eeac43a7a8e5a3d00ed2971e
dany1061 <spermazoide0@gmail.com>
2022-03-26 03:27:11 +0300

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+///using System.Timers;
+using System.IO;
+using System.ServiceProcess;
+using Microsoft.Win32;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+     ///   private readonly Timer _timer;
+
+        public Class1()
+        {
+     ///       _timer = new Timer(1000) { AutoReset = true };
+     ///       _timer.Elapsed += TimerElapsed;
+            log.Info("Service Initialized");
+        }
+
+        ServiceController[] servicesList = ServiceController.GetServices();
+
+    ///private void TimerElapsed(object sender, ElapsedEventArgs e)
+     ///   {
+     ///       string[] lines = new string[] { DateTime.Now.ToString() };
+     ///       File.AppendAllLines(@"C:\Users\Dany\source\repos\ConsoleApp1\ConsoleApp1\Heartbeat.txt", lines);
+     ///   }
+
+        public void Start()
+        {
+            ///_timer.Start();
+            foreach (ServiceController service in servicesList)
+            {
+                log.Info("Service Name: " + service.ServiceName);
+            }
+
+        }
+
+        public void Stop()
+        {
+     ///       _timer.Stop();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using System.IO;
+using System.ServiceProcess;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+
+            ///log.Error("This Is My error message");
+            Console.WriteLine("Dany + 1");
+            Console.ReadLine();
+
+            var exitCode = HostFactory.Run(x => ///This is  a usage of Topshelf for starting and stopping windows service
+            {
+                x.Service<Class1>(s =>
+                {
+                    s.ConstructUsing(class1 => new Class1());
+                    s.WhenStarted(class1 => class1.Start());
+                    s.WhenStopped(class1 => class1.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("Class1isService");
+                x.SetDisplayName("Class1 Display Service");
+                x.SetDescription("THis is just my service to test");
+
+            });
+            
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
